ddccff2d715eff05558a51ebcd3786e2c83ebb7d
Zoe <zoedroulias@gmail.com>
2026-01-17 14:25:52 -0500

    create/config flask app

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 from flask import Flask, render_template, redirect, url_for, request
+
+# create flask app
+app = Flask(__name__)
+
+# config for app
+app.config['SECRET_KEY'] = 'secret'
+app.config['SESSION_TYPE'] = 'filesystem'
