8f4e1dea65f7abd3d3f8917d7a4e59a407565ca2
Zoe <zoedroulias@gmail.com>
2026-01-18 08:05:00 -0500

    update verify_user() in model.py

--- model/model.py
+++ model/model.py
@@ -1,8 +1,13 @@
+import bcrypt
+import model.database.database as db
+
 def verify_user(email, password):
     # find user in database
+    user = db.user_by_email(email)
+    user_saved_pw = user['password']
     
     # check their password
-    
-    # return true if looged in, false otherwise
-    
-    pass
+    if bcrypt.checkpw(password.encode('utf-8'), user_saved_pw.encode('utf-8')):
+        return True
+    else:
+        return False
