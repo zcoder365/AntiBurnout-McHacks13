c980f3b9d201f13937d6e285251b035029ffe482
Larissa <lariss.cervantes@gmail.com>
2026-01-17 22:14:20 -0500

    draft of full code

---
+++ project/init_db.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+def init_db():
+#create a database if nonexistent
+    conn = sqlite3.connect("burnout.db")
+
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        email TEXT_UNIQUE,
+        password_hash TEXT,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS daily_inputs (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER
+        email TEXT_UNIQUE,
+        sleep_hours TEXT,
+        mood TEXT,
+        physical_activity TEXT,
+        cups_water FLOAT,
+        cups_caffeine FLOAT,
+        last_meal TIMESTAMP,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+
+    """)
+
+    conn.commit()
+    conn.close()
