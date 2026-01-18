576481c03d8fea91e3fd56d484c790efd1062eaa
Zoe <zoedroulias@gmail.com>
2026-01-17 22:20:26 -0500

    remove count_meals() from model.py

--- model/model.py
+++ model/model.py
@@ -1,19 +1,3 @@
-from datetime import datetime
-
-def count_meals(user_email):
-    meal_count = 0
-    
-    # find a user in the users database
-    
-    # get the user's ID
-    
-    # find the user's tracker form records
-    
-    # update meal count by counting how many records have a date/time in the last_meal col
-    
-    # return meal count
-    return meal_count
-
 def verify_user(email, password):
     # find user in database
     
